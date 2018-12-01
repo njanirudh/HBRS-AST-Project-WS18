06724e2834b0034c60a1ff39430d044666ea6252
Sasi Kiran <gsasikiran000@gmail.com>
2018-12-01 19:39:33 +0100

    Written template code for test case

---
+++ python/test_cases.py
@@ -0,0 +1,23 @@
+import unittest
+
+def test_1:
+    """
+    Checks if the input data type is correct or not
+    :return: Bool
+    """
+    in_value_1 = []
+    in_value_2 = str
+
+
+
+def test_2:
+    """
+    Checks if the output values are correct for given sensor input values.
+    :return: Bool
+    """
+    in_value_1 = []
+    in_value_2 = []
+
+if __name__ == "__main__":
+    test_1()
+    test_2()
